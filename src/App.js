9cfbf65e22f31ffd5e5ade9999f8956d8ad65b29
PoonamAgg <agarwalit09>
2024-01-10 12:02:49 +0530

    Shoppers_t-shirt_Project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import { SellerHead } from "./Components/SellerPage/SellerHead";
+import Screencart from "./Components/Userinterface/Screencart";
+
+function App() {
+  return (
+    <div className="App">
+       <SellerHead></SellerHead>
+       <Screencart></Screencart>
+    </div>
+  );
+}
+
+export default App;
